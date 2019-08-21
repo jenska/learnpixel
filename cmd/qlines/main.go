426ccabb437798cbfd9ba05d5c9d2eb2a584b4ab
Jens Kaiser <jens.kaiser.de@gmail.com>
2019-08-21 18:13:47 +0200

    refactored sources

--- cmd/qlines/main.go
+++ cmd/qlines/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/imdraw"
 	"github.com/faiface/pixel/pixelgl"
+	"github.com/jenska/learnpixel/pkg/core"
 )
 
 const (
@@ -17,41 +18,35 @@ const (
 	winWidth  = 1024
 )
 
-type (
-	qline struct {
-		line [2]pixel.Vec
-		vel  [2]pixel.Vec
-	}
-)
-
-func randomVec(b pixel.Rect) pixel.Vec {
-	return pixel.V(
-		rand.Float64()*(b.Max.X-b.Min.X)+b.Min.X,
-		rand.Float64()*(b.Max.Y-b.Min.Y)+b.Min.Y)
+type qline struct {
+	line [2]pixel.Vec
+	vel  [2]pixel.Vec
 }
 
-func moveVec(p, v *pixel.Vec) {
+func moveVec(p, v *pixel.Vec, b pixel.Rect) {
 	x, y := p.X, p.Y
 	vx, vy := v.X, v.Y
 
-	if x+vx < 0 || x+vx > winWidth {
+	if x+vx < 0 || x+vx > b.W() {
 		vx = -vx
 	}
-	if y+vy < 0 || y+vy > winHeight {
+	if y+vy < 0 || y+vy > b.H() {
 		vy = -vy
 	}
 
 	p.X, p.Y = x+vx, y+vy
 	v.X, v.Y = vx, vy
+	core.Clamp(p, b)
 }
 
 func run() {
 	bounds := pixel.R(0, 0, winWidth, winHeight)
 	vel := pixel.R(-velocity, -velocity, velocity, velocity)
 	cfg := pixelgl.WindowConfig{
-		Title:  "QLines Demo",
-		Bounds: bounds,
-		VSync:  true,
+		Title:     "QLines Demo",
+		Bounds:    bounds,
+		Resizable: true,
+		VSync:     true,
 	}
 	win, err := pixelgl.NewWindow(cfg)
 	if err != nil {
@@ -63,18 +58,18 @@ func run() {
 
 	var qlines [nlines]qline
 	top := &qlines[0]
-	top.line[0] = randomVec(bounds)
-	top.line[1] = randomVec(bounds)
-	top.vel[0] = randomVec(vel)
-	top.vel[1] = randomVec(vel)
+	top.line[0] = core.RandomVec(bounds)
+	top.line[1] = core.RandomVec(bounds)
+	top.vel[0] = core.RandomVec(vel)
+	top.vel[1] = core.RandomVec(vel)
 	for !win.Closed() {
 		win.SetClosed(win.JustPressed(pixelgl.KeyEscape))
 
 		imd.Clear()
 
 		copy(qlines[1:], qlines[0:nlines-2])
-		moveVec(&top.line[0], &top.vel[0])
-		moveVec(&top.line[1], &top.vel[1])
+		moveVec(&top.line[0], &top.vel[0], bounds)
+		moveVec(&top.line[1], &top.vel[1], bounds)
 		for _, m := range qlines {
 			imd.Push(m.line[0])
 			imd.Push(m.line[1])
@@ -83,6 +78,7 @@ func run() {
 		win.Clear(color.Black)
 		imd.Draw(win)
 		win.Update()
+		bounds = win.Bounds()
 	}
 }
 
